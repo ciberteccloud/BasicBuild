3bb77f63bb4129b0dce1437bb99471cca1f3b900
Juvega <juvega@gmail.com>
2016-12-29 17:06:11 -0500

    Organizando proyecto

---
+++ BasicUnitTest/BasicVerification.Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestBasicBuild;
+
+namespace BasicVerification.Test
+{
+    [TestClass]
+    public class BasicVerification
+    {
+        [TestMethod]
+        public void Basic_Unit_Test()
+        {
+            var basicTest = new Mensajes();
+            Assert.AreEqual("Hola Jenkins", $"{Mensajes.Greeting()} {Mensajes.Name()}");
+        }
+    }
+}
