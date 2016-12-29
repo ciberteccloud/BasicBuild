b4608d0721c698492d0feb4fe0902fef88064622
Juvega <juvega@gmail.com>
2016-12-29 15:12:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBasicBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Compilación via Jenkins");
+        }   
+             
+    }
+}
